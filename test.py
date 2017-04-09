b9cbee2ff93ae794784faaf63bb6d864b792ec52
rabid-ant <rabid-ant@users.noreply.github.com>
2017-04-09 13:26:58 +1200

    Sun

--- test.py
+++ test.py
@@ -4,13 +4,28 @@ import paho.mqtt.client as mqtt
 import configparser
 import logging
 import time
+import signal
+import sys
 
 config = configparser.ConfigParser()
 config.read('config.ini')
 
 logging.basicConfig(filename='example.log',level=logging.INFO)
 
-############## Start of MQTT Functionality ############
+def signal_handler(signal, frame):
+    print('Ctrl-C received. Program exiting')
+    print('Cleaning up')
+    #client.loop_stop()
+    sys.exit(0)
+
+####### Register handler for SIGINT graceful exit ######
+signal.signal(signal.SIGINT, signal_handler)
+
+####### Telegram - create updater and link it to bot, then dispatcher  #######
+updater = Updater(token=config['KEYS']['bot_token'])
+dispatcher = updater.dispatcher
+
+######## Start of MQTT Functionality ############
 # The callback for when the client receives a CONNACK response from the server.
 def on_connect(client, userdata, flags, rc):
     print("Connected with result code "+str(rc))
@@ -20,7 +35,7 @@ def on_connect(client, userdata, flags, rc):
 
 # The callback for when a PUBLISH message is received from the server.
 def on_message(client, userdata, msg):
-    print(msg.topic+" "+str(msg.payload))
+    print(time.strftime('%H:%M:%S')+" "+msg.topic+" "+str(msg.payload))
 
 client = mqtt.Client()
 client.username_pw_set(config['ADAFRUIT']['user_name'], password=config['ADAFRUIT']['api_token'])
@@ -29,7 +44,14 @@ client.on_message = on_message
 
 client.connect(config['MQTT_SERVER']['mqtt_url'], 1883, 60)
 client.loop_start()
-for x in range(1,10):
+
+
+
+# Infinite loop to keep program executing while multithreaded components carry on
+while True:
+    pass
+
+# Code snippets to keep
+'''for x in range(1,10):
     time.sleep(1) #Note that this does not block the MQTT loop at all. Yay!
-    print x
-client.loop_stop()
+    print x'''
